eb21759349455a1dd805c1ffd19984267af3253e
John L Chen <zuan@chromium.org>
2021-05-10 03:19:25 +0800

    Initial commit for the blank extension package.

---
+++ extensions/blank/in-gateway.mjs
@@ -0,0 +1,35 @@
+// Copyright 2021 HITCON Online Contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
+import assert from 'assert';
+
+/**
+ * This represents the in-gateway/in-area part of the extension.
+ * One instance of InGateway will be created for each gateway service
+ * launched.
+ */
+class InGateway {
+  /**
+   * Create the in-gateway extension object but does not start it.
+   * @constructor
+   * @param {ExtensionHelper} helper - An extension helper object for servicing
+   * various functionalities of the extension.
+   */
+  constructor(helper) {
+    void helper;
+    assert.fail('Not implemented');
+  }
+
+  /**
+   * Returns true if this extension have an in-gateway part.
+   * If this returns false, the constructor for InGateway will not be called.
+   * Otherwise, a InGateway object is instanciated for each gateway service
+   * instance launched.
+   * @return {Boolean} haveInGateway - See above.
+   */
+  static haveInGateway() {
+    return false;
+  }
+}
+
+export default InGateway;
