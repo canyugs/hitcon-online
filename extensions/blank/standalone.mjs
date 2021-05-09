d334135d1f8db7ffbd5782aa990dd50f6c6741b5
John L Chen <zuan@chromium.org>
2021-05-10 03:19:25 +0800

    Initial commit for the blank extension package.

---
+++ extensions/blank/standalone.mjs
@@ -0,0 +1,32 @@
+// Copyright 2021 HITCON Online Contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
+import assert from 'assert';
+
+/**
+ * This represents the standalone extension service for this extension.
+ */
+class Standalone {
+  /**
+   * Create the standalone extension service object but does not start it.
+   * @constructor
+   * @param {ExtensionHelper} helper - An extension helper object for servicing
+   * various functionalities of the extension.
+   */
+  constructor(helper) {
+    void helper;
+    assert.fail('Not implemented');
+  }
+
+  /**
+   * Returns true if this extension have a standalone part.
+   * If this returns false, the constructor for Standalone will not be called.
+   * Otherwise, a Standalone object is instanciated.
+   * @return {Boolean} haveStandalone - See above.
+   */
+  static haveStandalone() {
+    return false;
+  }
+}
+
+export default Standalone;
