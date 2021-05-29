7704fb3183e1c2c556f1df3bd80748afabcadb50
John L Chen <zuan@chromium.org>
2021-05-29 23:08:35 +0800

    Add player and gateway service registration to RPC Directory

--- common/rpc-directory/directory.mjs
+++ common/rpc-directory/directory.mjs
@@ -35,6 +35,15 @@ class Directory {
   }
 
   /**
+   * Part of the constructor that needs to be async.
+   * This is created because constructor can't be async.
+   * Note that this is called right after the constructor.
+   */
+  async asyncConstruct() {
+    // Nothing here.
+  }
+
+  /**
    * Create the real redis client and create the async functions.
    */
   _createRealRedis() {
@@ -45,6 +54,7 @@ class Directory {
     this.redis.publishAsync = promisify(this.redis.publish);
     this.redis.hsetAsync = promisify(this.redis.hset);
     this.redis.hgetAsync = promisify(this.redis.hget);
+    this.redis.hgetallAsync = promisify(this.redis.hgetall);
   }
 
   /**
@@ -65,19 +75,142 @@ class Directory {
    * service.
    * @param {string} name - Name of the gateway service.
    */
-  addGatewayServiceName(name) {
-    void name;
-    assert.fail('Not implemented');
-    return undefined;
+  async addGatewayServiceName(name) {
+    let ret = await getRedis().hsetAsync(['gatewayServers', name, name]);
+    if (ret !== 1) {
+      throw 'Failed to add gateway service name to redis: '+ret;
+    }
   }
   
   /**
    * Return the list of gateway service service name.
    * @param {object} arr - An array of gateway service name.
    */
-  getGatewayServices() {
-    assert.fail('Not implemented');
-    return [];
+  async getGatewayServices() {
+    let ret = await getRedis().hgetallAsync('gatewayServers');
+    let result = [];
+    for (const p in ret) {
+      result.push(p);
+    }
+    return result;
+  }
+
+  /**
+   * Return the redis key for the corresponding player.
+   * @param {string} playerName - The player's ID.
+   */
+  _getPlayerKey(playerName) {
+    return 'p-'+playerName;
+  }
+
+  /**
+   * Register the gateway service handling the user/player.
+   * This is called by gateway service when the player connects.
+   * @param {string} playerName - The player's ID.
+   * @param {string} serviceName - The service
+   * @return {boolean} success - True if successful, and player may proceed.
+   * False if someone else is already logged in as that user.
+   */
+  async registerPlayer(playerName, serviceName) {
+    // NX: Only set if it doesn't exist.
+    let ret = await getRedis().setAsync(
+        [this._getPlayerKey(playerName), serviceName, 'NX']);
+    if (ret === null) {
+      // Player already connected with another connection.
+      return false;
+    }
+    if (ret === 'OK') {
+      // Set correctly.
+      // TODO: Notify other services with pub/sub?
+      return true;
+    }
+    // Shouldn't happen.
+    console.assert('Invalid reply from redis in registerPlayer: '+ret);
+    return false;
+  }
+
+  /**
+   * Unregister the gateway service handling the user/player.
+   * This is called by gateway service when the player disconnects.
+   * @param {string} playerName - The player's ID.
+   * @param {string} serviceName - The service
+   */
+  async unregisterPlayer(playerName, serviceName) {
+    // Get the key to check that we're indeed the service holding the user.
+    let ret = await getRedis().getAsync([this._getPlayerKey(playerName)]);
+    if (ret !== serviceName) {
+      throw (`Service ${serviceName} trying to unregister ${playerName} ` +
+          `ownered by ${ret}`);
+    }
+    ret = await getRedis().delAsync([this._getPlayerKey(playerName)]);
+    if (ret !== 1) {
+      throw `${ret} keys deleted when trying to unregister ${playerName}`;
+    }
+    await this.storage.unloadData(this._getPlayerDataName(playerName));
+  }
+
+  /**
+   * Return the data name for use in DataStore for the corresponding player.
+   * @param {string} playerName - The player's ID.
+   */
+  _getPlayerDataName(playerName) {
+    return `p-${playerName}`;
+  }
+  
+  /**
+   * Return the stored data for player.
+   * @param {string} playerName - The player's ID.
+   * @return {object} playerData - The player's data. It's format is in
+   * ensurePlayerData().
+   */
+  async getPlayerData(playerName) {
+    const dataName = this._getPlayerDataName(playerName);
+    let data = await this.storage.loadData(dataName);
+    data = ensurePlayerData(data);
+    return data;
+  }
+  
+  /**
+   * Save the player's data.
+   * @param {string} playerName - The player's ID.
+   * @return {object} playerData - The player's data. It's format is in
+   * ensurePlayerData().
+   */
+  async setPlayerData(playerName, playerData) {
+    const dataName = this._getPlayerDataName(playerName);
+    let data = await this.storage.saveData(dataName, playerData);
+  }
+
+  /**
+   * Initialize the player data if it's not initialized.
+   * @param {string} playerName - The player's ID.
+   * @param {object} playerData - The player's data.
+   * @return {object} playerData - The player's data that is initialized.
+   */
+  ensurePlayerData(playerName, playerData) {
+    function setDefaultValue(obj, key, val) {
+      if (!(key in obj)) {
+        key[obj] = val;
+      }
+    }
+    // {string} playerName - The player's ID. The primary key used in our
+    // system.
+    setDefaultValue(playerData, 'playerName', playerName);
+    // {string} displayName - The name to show for the player on screen.
+    setDefaultValue(playerData, 'displayName', playerName);
+
+    // TODO: Set them to a default specified by assets.json.
+    // {string} displayChar - The graphic for the character. This is used and
+    // passed to GraphicAssets.
+    setDefaultValue(playerData, 'displayChar', 'char1');
+
+    // TODO: Set them to a spawn point specified by map.json.
+    // {Number} x - Character's x coordinate.
+    setDefaultValue(playerData, 'x', 5);
+    // {Number} y - Characetr's y coordinate.
+    setDefaultValue(playerData, 'y', 5);
+
+    return playerData;
   }
 
   /**
