f81f228d374a8fa061d90d6effd2978f6aac81cb
John L Chen <zuan@chromium.org>
2021-05-31 02:49:29 +0800

    Add APIs for other extensions in ExtensionHelper and other misc addition

--- common/extlib/extension-helper.mjs
+++ common/extlib/extension-helper.mjs
@@ -21,14 +21,14 @@ class ExtensionHelper {
   }
 
   /**
-   * Register an interaction. That is, if the user clicks on the map location
+   * Register an interaction. That is, if the player clicks on the map location
    * (x, y), then callback will be called.
    * @param {Number} x - The map x coordinate.
    * @param {Number} y - The map y coordinate.
-   * @param {function} callback - The callback when user interact with the
+   * @param {function} callback - The callback when player interact with the
    * specified tile in the map. Its prototype is:
-   * async function (user)
-   * Where user is an User object, who clicked/interacted with the map tile.
+   * async function (player)
+   * Where player is an Player object, who clicked/interacted with the map tile.
    */
   registerInteraction(x, y, callback) {
     void [x, y, callback];
@@ -36,37 +36,79 @@ class ExtensionHelper {
   }
 
   /**
-   * Return the list of all user IDs.
-   * @return {object} An array of String, the user IDs.
+   * Return the list of all player IDs.
+   * @return {object} An array of String, the player IDs.
    */
-  listUserID() {
+  listPlayerID() {
     assert.fail('Not implemented');
     return [];
   }
 
   /**
-   * Return the User object for the corresponding user ID.
-   * @param {String} uid - The user ID.
-   * @return {User} user - The User object for interacting with the user.
+   * Return the Player object for the corresponding user ID.
+   * @param {String} playerID - The player's ID.
+   * @return {Player} player - The Player object for interacting with the user.
    */
-  getUser(uid) {
-    void uid;
+  getPlayer(playerID) {
+    void playerID;
     assert.fail('Not implemented');
     return undefined;
   }
 
   /**
-   * Register an API for users to call.
+   * Register an API for users to call. This can be called by code from the
+   * browser side.
    * This should only be called from the Standalone extension service.
    * In-gateway part of the extension should not export any APIs.
    * @param {String} methodName - The name of the method.
    * @param {function} callback - The callback to execute. Its signature is:
-   * async function (user, args)
-   * Whereby user is the User object and args is an object.
+   * async function (player, args)
+   * Whereby player is the Player object and args is an object.
    * It returns another object that is the result.
    */
-  registerUserAPI(methodName, callback) {
+  registerPlayerAPI(methodName, callback) {
     void [methodName, callback];
+    assert.fail('Not implemented');
+  }
+
+  /**
+   * Call an API on the browser side.
+   * @param {String} playerID - The ID of the player to call.
+   * @param {String} extensionName - Name of the extension.
+   * @param {String} methodName - Name of the method.
+   * @param {object} args - The arguments.
+   * @param {Number} timeout - An optional timeout in ms.
+   * @return {object} result - The result from the call.
+   */
+  callPlayerAPI(playerID, extensionName, methodName, args, timeout) {
+    void [playerID, extensionName, methodName, args, timeout];
+    assert.fail('Not implemented');
+  }
+
+  /**
+   * Register an API for other extensions to call.
+   * This should only be called from the Standalone extension service.
+   * In-gateway part of the extension should not export any APIs.
+   * @param {String} methodName - The name of the method.
+   * @param {function} callback - The callback to execute. Its signature is:
+   * async function (args)
+   * Whereby user is the Player object and args is an object.
+   * It returns another object that is the result.
+   */
+  registerExtensionAPI(methodName, callback) {
+    void [methodName, callback];
+    assert.fail('Not implemented');
+  }
+
+  /**
+   * Call the API of another extension.
+   * @param {String} extensionName - The name of the extension.
+   * @param {String} methodName - The name of the method.
+   * @param {object} args - The arguments to the API.
+   * @return {object} result - The result from the call.
+   */
+  callExtensionAPI(extensionName, methodName, args) {
+    void [extensionName, methodName, args];
     assert.fail('Not implemented');
   }
 
