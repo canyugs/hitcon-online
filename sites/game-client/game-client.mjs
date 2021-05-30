e355809c3a7a15a22db264aa9470b808b9a76e5a
John L Chen <zuan@chromium.org>
2021-05-30 18:18:12 +0800

    Add game state transfer and input handling in client

--- sites/game-client/game-client.mjs
+++ sites/game-client/game-client.mjs
@@ -13,9 +13,13 @@ class GameClient {
    * state.
    * @constructor
    */
-  constructor(socket, gameState) {
+  constructor(socket, gameState, inputManager) {
     this.socket = socket;
     this.gameState = gameState;
+    this.inputManager = inputManager;
+    this.gameStarted = false;
+    // playerInfo stores information regarding the current player.
+    this.playerInfo = {};
     // This function is called if server disconnects us or if any fatal
     // error occurs.
     this.disconnectCallback = undefined;
@@ -49,14 +53,79 @@ class GameClient {
       socket.on('gameStart', (msg) => {
         this.onStartup(msg);
       });
+      socket.on('stateTransfer', (msg) => {
+        this.gameState.acceptStateTransfer(msg);
+        console.log('State transfer done!');
+      });
+      socket.on('location', (msg) => {
+        this.gameState.onLocation(msg);
+      });
     });
   }
 
   /**
-   * This is called when we're authenticated, and ready to start the game.
+   * This is called when we're authenticated and ready to start the game.
+   * @param {object} msg - The game start message from server.
    */
-  async onStartup() {
+  async onStartup(msg) {
+    if (this.gameStarted) {
+      console.assert('Duplicate game start event.');
+      return;
+    }
+    let pi = this.playerInfo;
+    pi.playerID = msg.playerData.playerID;
+    pi.displayName = msg.playerData.displayName;
+    pi.displayChar = msg.playerData.displayChar;
+    let p = this.gameState.getPlayer(pi.playerID);
+    [pi.x, pi.y, pi.facing] = [p.x, p.y, p.facing];
     console.log('Game starting');
+
+    this.gameStarted = true;
+    this.gameState.registerPlayerLocationChange((loc) => {
+      if (loc.playerID == this.playerInfo.playerID) {
+        [this.playerInfo.x, this.playerInfo.y, this.playerInfo.facing] =
+            [loc.x, loc.y, loc.facing];
+      }
+    });
+    this._initializeInputs();
+  }
+
+  /**
+   * Initialize the inputs for the game.
+   */
+  _initializeInputs() {
+    this.inputManager.onMove((direction) => {
+      this.onDirection(direction);
+    });
+  }
+
+  /**
+   * This is called when user presses a direction key.
+   * @param {string} direction - 'U', 'D', 'L', R'
+   */
+  async onDirection(direction) {
+    console.log(`On direction ${direction}`);
+    if (direction == 'U') {
+      await this.moveTo(this.playerInfo.x, this.playerInfo.y-1, 'U');
+    } else if (direction == 'D') {
+      await this.moveTo(this.playerInfo.x, this.playerInfo.y+1, 'D');
+    } else if (direction == 'L') {
+      await this.moveTo(this.playerInfo.x-1, this.playerInfo.y, 'L');
+    } else if (direction == 'R') {
+      await this.moveTo(this.playerInfo.x+1, this.playerInfo.y, 'R');
+    }
+  }
+
+  /**
+   * Emit message to move to a location.
+   * @param {Number} x
+   * @param {Number} y
+   * @param {string} facing
+   */
+  async moveTo(x, y, facing) {
+    let loc = {};
+    [loc.x, loc.y, loc.facing] = [x, y, facing];
+    this.socket.emit('location', loc);
   }
 
   /**
