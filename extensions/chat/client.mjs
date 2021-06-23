6bdf2a8b3b97780944c754dc294d6bc4c2dc880a
zeze-zeze <zezectf@gmail.com>
2021-06-23 13:20:08 +0000

    1. chat server demo version done 2. fix bugs 3. delete unused auth/test_app

---
+++ extensions/chat/client.mjs
@@ -0,0 +1,45 @@
+// Copyright 2021 HITCON Online Contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
+/**
+ * This class is the browser/client side of an extension.
+ * One instance is created for each connected player.
+ */
+class Client {
+  /**
+   * Create the client side of the extension.
+   * @constructor
+   * @param {ClientExtensionHelper} helper - An extension helper object for
+   * servicing various functionalities of the extension.
+   */
+  constructor(helper) {
+    this.helper = helper;
+    console.assert('Not implemented');
+    document.getElementById('chat_message').addEventListener('keypress', this.send_msg(this));
+  }
+
+  send_msg(client){
+    return function(evt){
+      if(evt.keyCode == 13){
+        let id = document.getElementById('chat_message');
+        client.helper.callStandaloneAPI('broadcastMessage', {'msg': id.value}, 5000);
+        id.value = '';
+      }
+    }
+  }
+
+  onExtensionBroadcast(arg){
+    document.getElementById('message_history').innerHTML += '<span>' + encodeURI(arg.msg) + '</span><br>';
+  }
+
+  /**
+   * Returns true if this extension have a browser side part.
+   * If this returns false, the constructor for Client will not be called.
+   * @return {Boolean} haveClient - See above.
+   */
+  static haveClient() {
+    return false;
+  }
+};
+
+export default Client;
