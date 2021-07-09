44405d43d26dfe70cb43508e28f618b3e046f328
Allen Chou <s3131212@gmail.com>
2021-07-09 17:30:36 +0800

    Draft for standalone extension and gRPC

--- services/standalone/standalone-extension.mjs
+++ services/standalone/standalone-extension.mjs
@@ -21,15 +21,12 @@ import SingleProcessRPCDirectory from
   '../../common/rpc-directory/single-process-RPC-directory.mjs';
 import MultiProcessRPCDirectory from
   '../../common/rpc-directory/multi-process-RPC-directory.mjs';
-import AssetServer from './asset-server.mjs';
-import AuthServer from '../auth/AuthServer.mjs';
 
 /* Import all utility classes */
 import GameMap from '../../common/maplib/map.mjs';
-import GameState from '../../common/maplib/game-state.mjs';
 import ExtensionManager from '../../common/extlib/extension-manager.mjs';
 
-async function mainServer() {
+async function standaloneExtensionServer() {
   /* Redis integration */
   // TODO: Uncomment once configuration for redis is done.
   // redisClient = redis.createClient();
@@ -43,8 +40,7 @@ async function mainServer() {
   const io = new Server(server);
 
   /* Create all utility classes */
-  //const rpcDirectory = new SingleProcessRPCDirectory();
-  const rpcDirectory = new MultiProcessRPCDirectory(config.get('server').address + ':' + config.get('server').port);
+  const rpcDirectory = new MultiProcessRPCDirectory("localhost:5001");
   await rpcDirectory.asyncConstruct();
   // Load the map.
   const mapList = config.get("map");
@@ -52,37 +48,17 @@ async function mainServer() {
   const mapJSON = JSON.parse(rawMapJSON);
   // We do not have GraphicAsset on the server side.
   const gameMap = new GameMap(undefined, mapJSON);
-  const gameState = new GameState(gameMap);
   const broadcaster = new AllAreaBroadcaster(io, rpcDirectory, gameMap);
-  const extensionManager = new ExtensionManager(rpcDirectory, broadcaster, gameMap, gameState);
+  const extensionManager = new ExtensionManager(rpcDirectory, broadcaster);
 
+  const extName = 'helloworld';
   await extensionManager.ensureClass('blank');
-  for (const extName of extensionManager.listExtensions()) {
-    await extensionManager.createExtensionService(extName);
-  }
+  await extensionManager.createExtensionService(extName);
+  await extensionManager.startExtensionService(extName);
 
-  /* Create all services */
-  const assetServer = new AssetServer(app, extensionManager);
-  const authServer = new AuthServer(app);
-  const gatewayService = new GatewayService(rpcDirectory, gameMap, authServer,
-      broadcaster, extensionManager);
+  const port = 5001;
+  await rpcDirectory.createGrpcServer(port);
 
-  /* Initialize static asset server */
-  await assetServer.initialize();
-  /* Start static asset server */
-  assetServer.run();
-  await broadcaster.initialize();
-  await gatewayService.initialize();
-  authServer.run();
-  for (const extName of extensionManager.listExtensions()) {
-    await extensionManager.startExtensionService(extName);
-  }
-
-  gatewayService.addServer(io);
-
-  // TODO: Set the port once configuration is done.
-  console.log(`Server is listening on port ${config.get('server.port')} ...`);
-  server.listen(config.get('server.port'));
 }
 
-mainServer();
+standaloneExtensionServer();
