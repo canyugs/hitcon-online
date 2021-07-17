2fc25cb9ebaa3f8b5345b1bfee27155ba7f33f75
David Su <adshfly@gmail.com>
2021-07-17 20:09:21 +0800

    feature:  basic dialog extension

---
+++ extensions/dialog/in-gateway.mjs
@@ -0,0 +1,30 @@
+// Copyright 2021 HITCON Online Contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
+import assert from 'assert';
+
+/**
+ * This represents the in-gateway/in-area part of the extension.
+ * One instance of InGateway will be created for each gateway service
+ * launched.
+ */
+class InGateway {
+  /**
+   * Create the in-gateway extension object but does not start it.
+   * @constructor
+   * @param {ExtensionHelper} helper - An extension helper object for servicing
+   * various functionalities of the extension.
+   */
+  constructor(helper) {
+    this.helper = helper
+  }
+
+  /**
+   * This is called by gateway service to initialie the in gateway part of
+   * the extension.
+   */
+   async initialize() {
+  }
+}
+
+export default InGateway;
