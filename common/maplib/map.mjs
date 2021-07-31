d7e7138e38150e9ef4e1b9ba35e0d241b8e820ba
Lisa Wang <rwq987654321000@gmail.com>
2021-07-31 17:38:13 +0800

    Change SpawnPointList from one map to all maps

--- common/maplib/map.mjs
+++ common/maplib/map.mjs
@@ -123,6 +123,7 @@ class GameMap {
   constructor(asset, maps) {
     this.graphicAsset = asset;
     this._maps = new Map();
+    this.spawnPointList = [];
     for (const [mapName, map] of Object.entries(maps)) {
       this._maps.set(mapName, new _SingleGameMap(asset, map));
     }
@@ -235,10 +236,15 @@ class GameMap {
     return ret;
   }
 
-  getSpawnPoint(mapName){
-    return this._maps.get(mapName).getSpawnPoint();
+  getSpawnPoint(){
+    if( this.spawnPointList.length === 0){
+      this._maps.forEach((map) => {
+        this.spawnPointList.concat(map.getSpawnPoint());
+      })
+    }
+    return this.spawnPointList;
   }
-
+}
 
 /**
  * The class that represents a single map.
@@ -456,7 +462,7 @@ class _SingleGameMap {
       const w = (cell.w ?? 1), h = (cell.h ?? 1);
       for (let i = 0; i < w; ++i) {
         for (let j = 0; j < h; ++j) {
-          expandList.push({x: cell.x + i, y: cell.y + j});
+          expandList.push({mapName: this.gameMap, x: cell.x + i, y: cell.y + j});
         }
       }
     }
@@ -468,10 +474,7 @@ class _SingleGameMap {
    * @return {list} spawn point - A list of the spawn point.
    */
   getSpawnPoint(){
-    if (this.spawnPointList == 0) {
-      this.spawnPointList = this.expandCellSet("SpawnPoint");
-    }
-    return this.spawnPointList;
+    return this.expandCellSet("SpawnPoint");
   }
 }
 
