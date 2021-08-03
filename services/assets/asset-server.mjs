3efe6e8914cee85079bb67620ff336de00ea4233
Allen Chou <s3131212@gmail.com>
2021-08-04 00:50:36 +0800

    Split gateway service and asset server to separated processes

---
+++ services/assets/asset-server.mjs
@@ -0,0 +1,97 @@
+// Copyright 2021 HITCON Online Contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
+// Boilerplate for getting the __dirname.
+import url from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+// Boilerplate for getting require() in es module.
+import {createRequire} from 'module';
+const require = createRequire(import.meta.url);
+
+const express = require('express');
+
+/**
+ * Asset server serves static and dynamic assets.
+ */
+class AssetServer {
+  /**
+   * Create an asset server, but doesn't start it.
+   * @constructor
+   * @param {App} app - An express app or router compatible with express.js.
+   * @param {ExtensionManager} extMan - An extension manager.
+   */
+  constructor(app, extMan) {
+    this.app = app;
+    this.extMan = extMan;
+  }
+
+  /**
+   * Initialize and start the asset server.
+   */
+  async initialize() {
+    this.staticRoutes();
+    await this.clientRoutes();
+    this.extRoutes();
+  }
+
+  /**
+   * Prepare the static routes.
+   */
+  staticRoutes() {
+    // TODO: Restrict the visible pages.
+    // Not sure if all static files are in sites
+    this.app.use('/static/sites', express.static(__dirname + '/../../sites/'));
+    this.app.use('/static/common', express.static(__dirname + '/../../common/'));
+    this.app.use('/static/run/map', express.static(__dirname + '/../../run/map'));
+  }
+
+  /**
+   * Prepare the route for serving the client.
+   */
+  async clientRoutes() {
+    // We're using ejs;
+    this.app.set('view engine', 'ejs');
+
+    // Send the user to the game client page.
+    this.app.get('/', (req, res) => {
+      res.redirect('/client.html');
+    });
+
+    // Prepare the client params beforehand.
+    this.clientParams = {};
+    let partials = await this.extMan.collectPartials(this.extMan.listExtensions());
+    this.clientParams.inDiv = [];
+    if (typeof partials.inDiv == 'object') {
+      this.clientParams.inDiv = partials.inDiv;
+    }
+    this.app.get('/client.html', (req, res) => {
+      res.render(path.resolve(__dirname + '/../../sites/game-client/client.ejs'), this.clientParams);
+    });
+    this.app.get('/extension/:extName', (req, res) => {
+      /* waf */
+      const extName = req.params.extName;
+      if (/^[a-zA-Z0-9_]+$/.test(extName) === false) {
+        res.status(404).send('Extension Not Found');
+        return;
+      }
+      res.sendFile(path.resolve(__dirname + `/../../extensions/${extName}/client.mjs`));
+    });
+  }
+
+  /**
+   * Prepare the routes for extensions.
+   */
+  extRoutes() {
+    this.app.get('/list_extensions', (req, res) => {
+      res.json(this.extMan.listExtensions());
+    });
+  }
+
+  run() {
+  }
+}
+
+export default AssetServer;
--- services/main/asset-server.mjs
+++ services/main/asset-server.mjs
@@ -1,93 +1,49 @@
 // Copyright 2021 HITCON Online Contributors
 // SPDX-License-Identifier: BSD-2-Clause
 
-// Boilerplate for getting the __dirname.
-import url from 'url';
-import fs from 'fs';
-import path from 'path';
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
 // Boilerplate for getting require() in es module.
 import {createRequire} from 'module';
 const require = createRequire(import.meta.url);
 
 const express = require('express');
+const path = require('path');
+const config = require('config');
+const redis = require('redis');
+const {Server} = require('socket.io');
+const http = require('http');
+const argv = require('minimist')(process.argv.slice(2));
 
-/**
- * Asset server serves static and dynamic assets.
- */
-class AssetServer {
-  /**
-   * Create an asset server, but doesn't start it.
-   * @constructor
-   * @param {App} app - An express app or router compatible with express.js.
-   * @param {ExtensionManager} extMan - An extension manager.
-   */
-  constructor(app, extMan) {
-    this.app = app;
-    this.extMan = extMan;
-  }
+import fs from 'fs';
 
-  /**
-   * Initialize and start the asset server.
-   */
-  async initialize() {
-    this.staticRoutes();
-    await this.clientRoutes();
-    this.extRoutes();
-  }
+import AssetServer from '../assets/asset-server.mjs';
+import ExtensionManager from '../../common/extlib/extension-manager.mjs';
 
-  /**
-   * Prepare the static routes.
-   */
-  async staticRoutes() {
-    // TODO: Restrict the visible pages.
-    // Not sure if all static files are in sites
-    this.app.use('/static/sites', express.static(__dirname + '/../../sites/'));
-    this.app.use('/static/common', express.static(__dirname + '/../../common/'));
-    this.app.use('/static/run/map', express.static(__dirname + '/../../run/map'));
-    const allExtensionNames = await fs.promises.readdir(__dirname + '/../../extensions');
-    for (let extensionName of allExtensionNames) {
-      this.app.use(`/static/extensions/${extensionName}`, express.static(__dirname + `/../../extensions/${extensionName}/common`));
-    }
-  }
+async function mainServer() {
+  /* Redis integration */
+  // TODO: Uncomment once configuration for redis is done.
+  // redisClient = redis.createClient();
+  // redisClient.on('error', (err) => {
+  //   console.error(err);
+  // });
 
-  /**
-   * Prepare the route for serving the client.
-   */
-  async clientRoutes() {
-    // We're using ejs;
-    this.app.set('view engine', 'ejs');
+  /* Create the http service */
+  const app = express();
+  const server = http.createServer(app);
 
-    // Send the user to the game client page.
-    this.app.get('/', (req, res) => {
-      res.redirect('/client.html');
-    });
+  /* Create services */
+  const extensionManager = new ExtensionManager(null, null, null, null);
+  const assetServer = new AssetServer(app, extensionManager);
 
-    // Prepare the client params beforehand.
-    this.clientParams = {};
-    let partials = await this.extMan.collectPartials(this.extMan.listExtensions());
-    this.clientParams.inDiv = [];
-    if (typeof partials.inDiv == 'object') {
-      this.clientParams.inDiv = partials.inDiv;
-    }
-    this.app.get('/client.html', (req, res) => {
-      res.render(path.resolve(__dirname + '/../../sites/game-client/client.ejs'), this.clientParams);
-    });
-  }
+  /* Initialize static asset server */
+  await assetServer.initialize();
+  /* Start static asset server */
+  assetServer.run();
 
-  /**
-   * Prepare the routes for extensions.
-   */
-  extRoutes() {
-    this.app.get('/list_extensions', (req, res) => {
-      res.json(this.extMan.listExtensions());
-    });
-  }
-
-  run() {
-  }
+  // TODO: Set the port once configuration is done.
+  const serviceName = ('service-name' in argv) ? argv['service-name'] : Object.keys(config.get('assetServers'))[0];
+  const port = config.get('assetServers')[serviceName].port;
+  console.log(`Server is listening on port ${port} ...`);
+  server.listen(port);
 }
 
-export default AssetServer;
+mainServer();
