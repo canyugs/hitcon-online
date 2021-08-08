dbba72a3f46273027f35d97b208bbd40566cd53f
John L Chen <zuan@chromium.org>
2021-08-08 22:01:54 +0800

    Implement basic functionalities in Modal and single choice dialog

--- extensions/dialog/common/client.mjs
+++ extensions/dialog/common/client.mjs
@@ -1,6 +1,67 @@
 // Copyright 2021 HITCON Online Contributors
 // SPDX-License-Identifier: BSD-2-Clause
 
+import Modal from '/static/sites/game-client/ui/modal.mjs';
+
+const DIALOG_DIV = 'dialog-div';
+const DIALOG_MSG_DIV = 'dialog-msg';
+const DIALOG_SUBJECT_DIV = 'dialog-subject';
+const DIALOG_BUTTON_CONTAINER_DIV = 'dialog-btn-container';
+
+class DialogModal extends Modal {
+  constructor(mainUI) {
+    const dom = document.getElementById(DIALOG_DIV);
+    super(mainUI, dom);
+
+    this.msgDom = document.getElementById(DIALOG_MSG_DIV);
+    this.subjectDom = document.getElementById(DIALOG_SUBJECT_DIV);
+    this.btnContDom = document.getElementById(DIALOG_BUTTON_CONTAINER_DIV);    
+  }
+
+  /**
+   * Call to set the dialog to display a single choice dialog.
+   */
+  async displayAsSingleChoice(subject, message, buttonText) {
+    if (this.isActive()) {
+      console.warn('Dialog already active, cannot displayAsSingleChoice');
+      return false;
+    }
+
+    if (!subject) {
+      subject = 'Message';
+    }
+    if (!buttonText) {
+      buttonText = 'OK';
+    }
+    this.subjectDom.innerHTML = subject;
+    this.msgDom.innerHTML = message;
+    this.btnContDom.innerHTML = `<button id="dialog-btn-OK">${buttonText}</button>`;
+    const btnOK = document.getElementById('dialog-btn-OK');
+
+    const p = new Promise((resolve, reject) => {
+      btnOK.addEventListener('click', () => {
+        this.hide();
+        resolve(true);
+        return true;
+      });
+    });
+
+    this.show();
+
+    return await p;
+  }
+
+  /**
+   * Called after it's shown.
+   */
+  onPostShow() {
+    // Set the size.
+    this.setSize('80%', '25%');
+    this.setPosition('10%', '55%');
+    return true;
+  }
+};
+
 /**
  * This class is the browser/client side of an extension.
  * One instance is created for each connected player.
@@ -18,6 +79,13 @@ class Client {
   }
 
   /**
+   * Called on game start.
+   */
+  async gameStart() {
+    this.modal = new DialogModal(this.helper.mainUI);
+  }
+
+  /**
    * Get the NPC data from Server (npc.json)
    */
   getNpcData() {
@@ -29,15 +97,21 @@ class Client {
 
   /**
    * Show the single choice dialog to user.
-   * @param {object} arg - npc data.
+   *
+   * WARNING: Message is *not* sanitized before showing the user.
+   * Please ensure any client-controllable message is sanitized before
+   * calling to avoid XSS.
+   *
+   * @param {object} arg - Should contain the following:
+   * {String} subject - The subject of the dialog.
+   * {String} message - The message to show.
+   * {String} btnText - The label of the OK button.
    */
-  s2c_singleChoiceDialog(arg) {
+  async s2c_singleChoiceDialog(arg) {
     console.log(`[Dialog] Single Choice Dialog`);
 
-    document.getElementById('npcName').innerHTML = arg.name;
-    document.getElementById('npcSentence').innerHTML = arg.sentence;
-
-    //todo: show the pop-up dialog for user
+    return await this.modal.displayAsSingleChoice(
+      arg.subject, arg.message, arg.btnText);
   }
 
 
@@ -48,7 +122,7 @@ class Client {
   s2c_multiChoiceDialog(arg) {
     console.log(`[Dialog] Multi Choice Dialog`);
     let buttonString = '';
-    for (let choice of arg.choice) {
+    for (const choice of arg.choice) {
       buttonString += `<button id="button">${choice}</button>`;
     }
 
@@ -67,7 +141,6 @@ class Client {
   sendResultToServer(result) {
     this.helper.callC2sAPI(null, 'getResultFromClient', 5000, {'result': result});
   }
-
 };
 
 export default Client;
--- sites/game-client/ui/main-ui.mjs
+++ sites/game-client/ui/main-ui.mjs
@@ -10,6 +10,8 @@ const UIState = Object.freeze({
 });
 
 const STAGING_ID = 'staging-div';
+const MODAL_BG_ID = 'modal-background';
+const MODAL_CONT_ID = 'modal-container';
 const OVERLAY_DIV = new Map();
 OVERLAY_DIV.set(OverlayPosition.LEFT_TOP, 'overlay-topleft');
 OVERLAY_DIV.set(OverlayPosition.LEFT_BOTTOM, 'overlay-bottomleft');
@@ -31,12 +33,15 @@ class MainUI {
   constructor() {
     this.state = UIState.NORMAL_UI;
     this.overlays = new Map();
+    this.activeModal = undefined;
 
     window.addEventListener('resize', (evt) => { this._onResize(evt); });
     window.addEventListener('gameStart', (evt) => { this._onResize(evt); });
 
     // Cache the common used DOM elements.
     this.stagingDom = document.getElementById(STAGING_ID);
+    this.modalBgDom = document.getElementById(MODAL_BG_ID);
+    this.modalContDom = document.getElementById(MODAL_CONT_ID);
     this.overlayDom = {};
     for (const [pos, div] of OVERLAY_DIV.entries()) {
       this.overlayDom[pos] = document.getElementById(div);
@@ -44,13 +49,6 @@ class MainUI {
 
     this.toolbarDom = document.getElementById(TOOLBAR_ID);
   }
-
-  /**
-   * Return Modal class. It can called by extension.
-   * When having multiple modals want to show, it should stacked by priority.
-   * @param modal one of Modal.
-   */
-  showModal(modal) {}
 
   /**
    *  Return one of UIState.
@@ -112,6 +110,76 @@ class MainUI {
    */
   _restoreToStaging(dom) {
     this.stagingDom.appendChild(dom);
+  }
+
+  /**
+   * Enable the modal UI.
+   * i.e. Display the backdrop (#model-background) and container
+   * (#model-container).
+   */
+  _enableModal() {
+    this.state = UIState.MODAL_SHOWN;
+    this.modalBgDom.classList.add('visible');
+    this.modalContDom.classList.add('visible');
+  }
+
+  /**
+   * Disable the modal UI.
+   * i.e. Remove the backdrop and hide the container.
+   */
+  _disableModal() {
+    this.state = UIState.NORMAL_UI;
+    this.modalBgDom.classList.remove('visible');
+    this.modalContDom.classList.remove('visible');
+  }
+
+  /**
+   * Reset all elements of the modal container into the staging container.
+   */
+  _evictModalContainer() {
+    // We look at children and not childNode because we only care about
+    // the DOM elements. We do not place text or other stuff in the modal
+    // container.
+    for (const ele of this.modalContDom.children) {
+      this._restoreToStaging(ele);
+    }
+    this.modalContDom.innerHTML = '';
+  }
+
+  /**
+   * This is called by the Modal class to set the modal class as the current
+   * active modal.
+   */
+  _setModal(modal) {
+    if (this.activeModal !== undefined) {
+      // TODO: Maybe support having a stack of to be activated modals?
+      console.warn('We do not currently support multiple active modals');
+      return false;
+    }
+
+    this._evictModalContainer();
+    this._enableModal();
+    this.activeModal = modal;
+    for (const ele of modal._getModalDOM()) {
+      console.log("Adding ");
+      console.log(ele);
+      this.modalContDom.append(ele);
+    }
+    return true;
+  }
+
+  /**
+   * This is called by the Modal class to remove itself as the active modal.
+   */
+  _clearModal(modal) {
+    if (this.activeModal !== modal) {
+      throw 'Incorrect modal trying to call _clearModal()';
+      return false;
+    }
+    this.activeModal = undefined;
+    this._evictModalContainer();
+    this._disableModal();
+    return true;
   }
 
   /**
