6a67cc8992e42c19097810ea7b9b9ad943291a55
Fanlan <fanlan1210@users.noreply.github.com>
2021-08-28 17:25:40 +0800

    create basic error modal UI

---
+++ sites/game-client/ui/error-modal.mjs
@@ -0,0 +1,24 @@
+// Copyright 2021 HITCON Online Contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
+import Modal from './modal.mjs';
+
+const ERROR_MODAL_DIV = 'error-modal';
+const ERROR_MESSAGE_DIV = 'error-message';
+const ERROR_HANDLER_MESSAGE_DIV = 'error-handler-message';
+
+class ErrorModal extends Modal {
+
+  constructor(mainUI) {
+    const dom = document.getElementById(ERROR_MODAL_DIV);
+    super(mainUI, dom);
+    this.errorMessageDom = document.getElementById(ERROR_MESSAGE_DIV);
+    this.handlerMessageDom = document.getElementById(ERROR_HANDLER_MESSAGE_DIV);
+  }
+
+  canDismiss() {
+    return false;
+  }
+}
+
+export default ErrorModal;
