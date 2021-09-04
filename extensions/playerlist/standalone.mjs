8659875d0a02b2fca84b0a8bae73b4fa0dbec0c5
zeze-zeze <zezectf@gmail.com>
2021-09-04 10:42:39 +0000

    Add TODO, delete unused, modify toolbar button

--- extensions/playerlist/standalone.mjs
+++ extensions/playerlist/standalone.mjs
@@ -24,16 +24,6 @@ class Standalone {
   }
 
   /**
-   * Send player list from gameState to client side.
-   * @c2s_getPlayerList
-   * @param {Player} player - player information
-   */
-  async c2s_getPlayerList(player) {
-    const playerIDs = [...this.helper.gameState.players.keys()];
-    await this.helper.callS2cAPI(player.playerID, 'playerlist', 'showPlayerList', 1000, playerIDs);
-  }
-
-  /**
    * Return the ejs partials for the client part of this extension.
    * @return {object} partials - An object, it could have the following:
    * inDiv - A string to the path of ejs partial for the location inDiv.
