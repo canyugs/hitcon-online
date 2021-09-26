aca611d666cd5bd1b0aab8b61aeaf6afdbd376b0
zeze-zeze <zezectf@gmail.com>
2021-09-26 14:17:11 +0000

    change lowercase

--- extensions/MapWatermark/common/client.mjs
+++ extensions/MapWatermark/common/client.mjs
@@ -42,7 +42,7 @@ class Client {
       this.helper.mapRenderer.registerCustomizedLayerToDraw(
         LAYER_MAPWATERMARK.zIndex, 
         LAYER_MAPWATERMARK.layerName, 
-        '_drawWaterMark', 
+        '_drawWatermark', 
         mapWatermarks
       );
     }).catch((err_msg) => {
