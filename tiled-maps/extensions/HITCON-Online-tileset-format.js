a38d8d7581184354069c0d3fe6e289887253458d
CanYu <fineaisa@gmail.com>
2021-10-10 22:47:10 +0800

    setup basic directories for Tiled App

---
+++ tiled-maps/extensions/HITCON-Online-tileset-format.js
@@ -0,0 +1,37 @@
+// This example script for Tiled App
+// TODO change content to match our config
+var customTilesetFormat = {
+    name: "HITCON Onine tileset files",
+    extension: "json",
+
+    write: function(map, fileName) {
+        var m = {
+            startX: 0,
+            startY: 0,
+            width: map.width,
+            height: map.height,
+            layers: [],
+            cellSets: [],
+        };
+
+        for (var i = 0; i < map.layerCount; ++i) {
+            var layer = map.layerAt(i);
+            if (layer.isTileLayer) {
+                var rows = [];
+                for (y = 0; y < layer.height; ++y) {
+                    var row = [];
+                    for (x = 0; x < layer.width; ++x)
+                        row.push(layer.cellAt(x, y).tileId);
+                    rows.push(row);
+                }
+                m.push(rows);
+            }
+        }
+
+        var file = new TextFile(fileName, TextFile.WriteOnly);
+        file.write(JSON.stringify(m));
+        file.commit();
+    },
+}
+
+tiled.registerTilesetFormat("hitcon-tileset", customTilesetFormat)
