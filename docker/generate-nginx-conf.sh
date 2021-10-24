3f2de06bab71d48d12ffba1fd7d9083f0785ee60
Allen Chou <s3131212@gmail.com>
2021-10-25 03:47:57 +0800

    Fix some bugs in the production code

--- docker/generate-nginx-conf.sh
+++ docker/generate-nginx-conf.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if [ "/myapp/run/config/production.json" -nt "/etc/nginx/conf.d/default.conf" ]
+if [ "/run/config/production.json" -nt "/etc/nginx/conf.d/default.conf" ]
 then
     echo 'Generating config file...'
     apt-get update > /dev/null
     apt-get install --no-install-recommends --no-install-suggests -y python3 > /dev/null
 
-    cd /myapp/docker/
+    cd /app/docker/
     python3 generate-nginx-conf.py /etc/nginx/conf.d/default.conf
 
     apt-get remove --purge --auto-remove -y python3 > /dev/null
