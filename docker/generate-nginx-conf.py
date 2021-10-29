d1d846b0868beb1e75dbf0062e8bb252972e5dc5
Allen Chou <s3131212@gmail.com>
2021-10-29 14:52:34 +0800

    Move the config generation out of the container

--- docker/generate-nginx-conf.py
+++ docker/generate-nginx-conf.py
@@ -2,13 +2,19 @@ from pathlib import Path
 import json
 import sys
 
-config = json.loads(Path('/run/config/production.json').read_bytes())
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+    print('Usage: python3 generate-nginx-conf.py run_dir [config_name]')
+    print('For example, python3 generate-nginx-conf.py ../run production')
+    sys.exit()
+
+run_dir = sys.argv[1]
+config_name = sys.argv[2] if len(sys.argv) == 3 else 'production'
+
+config = json.loads(Path(f'{run_dir}/config/{config_name}.json').read_bytes())
 template = Path('./nginx-template.conf').read_text()
 
-config_loc = 'nginx.conf' if len(sys.argv) < 2 else sys.argv[1]
-
-Path(config_loc).touch()
-Path(config_loc).write_text(template.format(
+Path(f'{run_dir}/nginx.conf').touch()
+Path(f'{run_dir}/nginx.conf').write_text(template.format(
     publicAddress=config['publicAddress'],
     online='online:' + str(config['assetServer']['port']),
     gateways=''.join([f"\n        server online:{v['httpAddress'].split(':')[1]}; # {k}" for k, v in config['gatewayServers'].items()])
--- docs/docker.md
+++ docs/docker.md
@@ -7,7 +7,9 @@ cd docker
 docker-compose up -d
 ```
 
-Currently there are three containers, for `redis`, `nginx` (for reverse proxy and load balancing), and `online` (main container for hitcon-online). The default config file for hitcon-online is `production.json`. Make sure that multiprocess mode is enabled and `publicAddress` is assigned.
+Currently there are three containers, for `redis`, `nginx` (for reverse proxy and load balancing), and `online` (main container for hitcon-online). Make sure that multiprocess mode is enabled and `publicAddress` is assigned.
+
+The nginx config file *will not* be automatic generated. You have to do `python3 generate-nginx-conf.py run_dir [config name]` to generate one, and restart the nginx container to take effect.
 
 By default, only port 80 (from Nginx) is exposed to the host. One can access hitcon-online through `online.hitcon.org`. This is a temporary location, we might not use it in production.
 
