bc7b36f79c62322eb380fddf07e3cf6c97c61c8d
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2021-11-02 19:33:00 +0800

    fix move-check.mjs

--- common/gamelib/move-check.mjs
+++ common/gamelib/move-check.mjs
@@ -49,7 +49,13 @@ function _borderAndWallCheck(oldPlayerData, updateMessage, gameMap) {
   // check border
   const {width, height} = gameMap.getMapSize(updateMessage.mapCoord.mapName);
   const {x, y} = updateMessage.mapCoord;
-  if (!(0 <= x && x <= width && 0 <= y && y <= height)) {
+  if (!(0 <= x && x < width && 0 <= y && y < height)) {
+    return false;
+  }
+
+  // check wall
+  // TODO: check all the map coordinates between old position and new position
+  if (gameMap.getCell(updateMessage.mapCoord, 'wall')) {
     return false;
   }
 
