e7cfc6f9e5765f469f9d4781f390eaeb551ef78d
fanlan1210 <fanlan1210@users.noreply.github.com>
2021-11-07 16:39:49 +0800

    items zero amount fix, auto focus to canvas when login. * when amount of item equals zero, the items will not display.

--- sites/game-client/game-client.mjs
+++ sites/game-client/game-client.mjs
@@ -138,6 +138,7 @@ class GameClient {
 
     game.mapRendererOverlay.show(OverlayPosition.MAIN_VIEW);
     this.avatarSelectionPage.hide();
+    this.inputManager.focusedElement = this.mapRenderer.canvas;
 
     // Start the browser side of all extensions.
     this.extMan.startAllExtensionClient();
