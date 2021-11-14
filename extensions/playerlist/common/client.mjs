b21dd33387def7a5b6900965ace372a1bd416d8d
John L Chen <zuan@chromium.org>
2021-11-15 00:50:51 +0800

    Update the playerlist refresh to listen to player update

--- extensions/playerlist/common/client.mjs
+++ extensions/playerlist/common/client.mjs
@@ -28,48 +28,117 @@ class Client {
    */
   constructor(helper) {
     this.helper = helper;
+    // The cache for each of the player's entry in the player list.
+    // Keyed by playerID, value is an object with the following:
+    // dom - The DOM.
+    // displayName - The current display name that is being shown.
+    // Note that we need this because DOM access could be expensive.
+    this.doms = new Map();
   }
 
   async gameStart() {
     this.tab = new PlayerlistTab(this.helper.mainUI);
 
-    this.showPlayerList();
+    this.syncPlayerList(true);
+    this.helper.gameState.registerOnPlayerUpdate((msg) => {
+      this.handlePlayerUpdate(msg);
+    });
+    // Periodically ensure that the list is correct.
+    setInterval(() => {
+      this.syncPlayerList(false);
+    }, 60*1000);
   }
 
   /**
-   * Show player list on left top.
-   * @showPlayerList
+   * Process the update player event to update the player list.
    */
-  showPlayerList() {
-    const template = document.getElementById('playerlist-player').cloneNode(true);
-    // TODO(zeze-zeze): Optimize the way to update playerlist
-    setInterval(() => {
-      document.getElementById('playerlist').innerHTML = '';
-      this.helper.gameState.getPlayers().forEach(playerID => {
-        if (typeof playerID.displayName !== 'string' || !playerID.displayName.includes(document.getElementById("searchPlayer").value)) {
-          return;
-        }
-        const playerDOM = template.cloneNode(true);
-        playerDOM.setAttribute('id', playerID.playerID);
-        playerDOM.setAttribute('data-player-context-menu', playerID.playerID);
-        
-        // Sanitize XSS
-        const san_playerID = filterXSS(playerID.playerID);
-        const san_displayName = filterXSS(playerID.displayName);
+  handlePlayerUpdate(msg) {
+    if (msg.removed) {
+      this.removePlayer(msg.playerID);
+      return;
+    }
 
+    const p = this.helper.gameState.getPlayer(msg.playerID);
+    if (p) {
+      this.syncPlayer(p);
+    } else {
+      console.warn(`Got invalid player obj for ${msg.playerID} in playerlist.handlePlayerUpdate: `, p);
+    }
+  }
 
-        playerDOM.setAttribute('onclick', 'startPrivateMessage(\'' + san_playerID + '\')');
-        playerDOM.querySelector('.player-name').textContent = san_displayName;
-        // TODO: get user role;
-        playerDOM.querySelector('.player-role').textContent = 'unknown role';
-        // TODO: get user picture;
-        playerDOM.querySelector('.player-picture').setAttribute('src', 'https://via.placeholder.com/56x61');
-        // TODO(zeze-zeze): Keep a record of dom elements
-        // TODO(zeze-zeze): Move it to a separate function
-        document.getElementById('playerlist').appendChild(playerDOM);
-        // TODO(zeze-zeze): Use onchange callback to update playerlist instead of waiting 1 second
-     });
-    }, 1000);
+  /**
+   * Synchronize the player list to the entire gameState data.
+   */
+  async syncPlayerList(firstTry) {
+    const players = this.helper.gameState.getPlayers();
+    Array.from(this.doms.keys()).filter((k) => { return !(players.has(k)); })
+      .forEach((playerID) => {
+        // Usually this should not happen.
+        console.warn('Removing player in syncPlayer: ', playerID);
+        this.removePlayer(playerID);
+      });
+    Array.from(players.keys()).forEach((playerID) => {
+      const ret = this.syncPlayer(players.get(playerID));
+      if (ret && !firstTry) {
+        // Usually this should not happen.
+        console.warn('Updating player in syncPlayerList: ', playerID);
+      }
+    });
+  }
+
+  /**
+   * Ensure that the given player's information is correct in the player list.
+   * @return {Boolean} modified - True if modified.
+   */
+  syncPlayer(p) {
+    if (!(this.doms.has(p.playerID))) {
+      // Create the DOM.
+      this.createPlayer(p);
+      return true;
+    }
+    let modified = false;
+    const cached = this.doms.get(p.playerID);
+    if (cached.displayName !== p.displayName) {
+      // Need to update the displayName.
+      cached.dom.querySelector('.player-name').textContent = p.displayName;
+      cached.displayName = p.displayName;
+      modified = true;
+    }
+    return modified;
+  }
+
+  /**
+   * Add a player to the player list.
+   */
+  createPlayer(p) {
+    const dom = document.getElementById('playerlist-player').cloneNode(true);
+    dom.setAttribute('id', `playerlist-entry-${p.playerID}`);
+    dom.setAttribute('data-player-context-menu', p.playerID);
+
+    const cached = {};
+    cached.dom = dom;
+    cached.dom.querySelector('.player-name').textContent = p.displayName;
+    cached.displayName = p.displayName;
+
+    cached.dom.querySelector('.player-role').textContent = '';
+    cached.dom.querySelector('.player-picture').setAttribute('src', 'https://via.placeholder.com/56x61');
+
+    document.getElementById('playerlist').appendChild(cached.dom);
+    this.doms.set(p.playerID, cached);
+  }
+
+  /**
+   * Remove a player from the player list.
+   */
+  removePlayer(playerID) {
+    if (!(this.doms.has(playerID))) {
+      // Already removed.
+      return;
+    }
+    const cached = this.doms.get(playerID);
+    this.doms.delete(playerID);
+
+    cached.dom.remove();
   }
 }
 
