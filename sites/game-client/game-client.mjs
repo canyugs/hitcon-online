d3063a1914c2a5447ce82d6864da034605820ec3
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2021-11-25 21:31:09 +0800

    refactor PR #327

--- sites/game-client/game-client.mjs
+++ sites/game-client/game-client.mjs
@@ -145,7 +145,6 @@ class GameClient {
     game.mapRendererOverlay.show(OverlayPosition.MAIN_VIEW);
     this.avatarSelectionPage.hide();
     this.inputManager.focusedElement = this.mapRenderer.getInputEventDOM();
-    this.inputManager.hasStarted = true;
     this.mainUI.introModal.show();
 
     // Start the browser side of all extensions.
