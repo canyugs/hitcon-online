423b91bc66f43a11082e255233cba2abe531d9f1
Chi-Feng Tsai <ktpss95112@gmail.com>
2022-07-06 17:11:12 +0800

    add a new extension to provide information of network latency

---
+++ extensions/ping/standalone.mjs
@@ -0,0 +1,31 @@
+// Copyright 2022 HITCON Online Contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
+
+/**
+ * TODO: jsdoc
+ */
+class Standalone {
+  /**
+   * TODO: jsdoc
+   */
+  constructor(helper) {
+    this.helper = helper;
+  }
+
+  /**
+   * Initializes the extension.
+   */
+  async initialize() {
+  }
+
+  /**
+   * TODO: jsdoc
+   */
+  async c2s_ping(player) {
+    return {};
+  }
+}
+
+
+export default Standalone;
