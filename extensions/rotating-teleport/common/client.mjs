6f512b9276557438277be1ab445ef1bc66b9ec6b
Chi-Feng Tsai <ktpss95112@gmail.com>
2022-08-18 11:42:33 +0800

    use broadcastToAllUser to tell client to start animation

--- extensions/rotating-teleport/common/client.mjs
+++ extensions/rotating-teleport/common/client.mjs
@@ -3,11 +3,6 @@
 
 import {MapCoord} from '/static/common/maplib/map.mjs';
 
-const LAYER_NAME = 'rotatingTeleport';
-const SEPARATOR = '.';
-const ACTION_IN = 'in';
-const ACTION_OUT = 'out';
-
 const ROTATE_HALF_DURATION = 3; // unit: sec
 const ROTATE_ANGULAR_ACCELERATION = 360; // unit: deg / (sec^2)
 const ROTATE_TIME_FORWARD = 0;
@@ -81,88 +76,20 @@ class Client {
    */
   constructor(helper) {
     this.helper = helper;
-    this.teleportLookupTable = new Map(); // key: the ID of the portal, value: the destination
-
-    // If the player is being teleported, then there would be an entry in this map.
-    // key: playerID, value: the target place of teleportation
-    this.isBeingTeleported = new Map();
   }
 
   /**
    * The initialization function.
    */
   async gameStart() {
-    this.parseLayer();
-
-    // teleport the player if he/she steps on a portal
-    this.helper.gameState.registerOnPlayerUpdate((msg) => {
-      if (typeof msg.playerID !== 'string') return;
-      if (typeof msg.mapCoord !== 'object') return;
-
-      // if he/she is not being teleported, check if steps on a portal defined by our extension
-      const layerValue = this.helper.gameMap.getCell(msg.mapCoord, LAYER_NAME);
-      if (layerValue === null) return;
-      const [id, action] = layerValue.split(SEPARATOR);
-      if (action !== ACTION_IN) return;
-
-      // rotating teleport
-      const fromPlace = this.helper.gameState.getPlayer(msg.playerID).mapCoord.copy();
-      const targetPlace = this.teleportLookupTable.get(id);
-      this.tryStartRotatingTeleportAnimation(msg.playerID, fromPlace, targetPlace);
-    });
+    // If the player is being teleported, then there would be an entry in this map.
+    // key: playerID, value: the target place of teleportation
+    this.isBeingTeleported = new Map();
   }
 
-  /**
-   * Parse the configuration
-   * @return {Boolean} - successful or not
-   */
-  parseLayer() {
-    // parse the layers and build the lookup table
-    const inIDs = new Set();
-    for (const [mapName, map] of this.helper.gameMap.getMaps()) {
-      const {width, height} = map.getMapSize();
-      for (let x = 0; x < width; ++x) {
-        for (let y = 0; y < height; ++y) {
-          const coord = new MapCoord(mapName, x, y);
-          const layerValue = map.getCell(coord, LAYER_NAME);
-          if (layerValue === null) continue;
-          console.assert(typeof layerValue === 'string');
-          const [id, action] = layerValue.split(SEPARATOR);
-          switch (action) {
-            case ACTION_IN:
-              inIDs.add(id);
-              break;
-            case ACTION_OUT:
-              this.teleportLookupTable.set(id, coord);
-              break;
-            default:
-              console.error(`[rotating-teleport] invalid layer value ${layerValue}`);
-          }
-        }
-      }
-    }
-    // check
-    if (inIDs.size !== this.teleportLookupTable.size) {
-      console.error(`[rotating-teleport] has ${inIDs.size} 'in' portal but has ${this.teleportLookupTable.size} 'out' portal`);
-      return false;
-    }
-    for (const id of inIDs) {
-      if (!this.teleportLookupTable.has(id)) {
-        console.error(`[rotating-teleport] missing 'out' portal of 'in' portal ${id}`);
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  /**
-   * @param {String} playerID
-   * @param {MapCoord} fromPlace
-   * @param {MapCoord} targetPlace
-   */
-  async s2c_tryStartRotatingTeleportAnimation(playerID, fromPlace, targetPlace) {
-    this.tryStartRotatingTeleportAnimation(playerID, fromPlace, targetPlace);
+  onExtensionBroadcast(kwargs) {
+    const {playerID, fromPlace, toPlace} = kwargs;
+    this.tryStartRotatingTeleportAnimation(playerID, fromPlace, toPlace);
   }
 
   /**
@@ -179,6 +106,7 @@ class Client {
 
     // create a new object to prevent from polluting the source object
     targetPlace = MapCoord.fromObject(targetPlace);
+    fromPlace = MapCoord.fromObject(fromPlace);
 
     // save the destination
     this.isBeingTeleported.set(playerID, targetPlace);
@@ -190,6 +118,8 @@ class Client {
     rotator.start();
 
     player.setCustomGetDrawInfo((player_) => {
+      // TODO: move the ViewerPosition smoothly
+
       if (phase === ROTATE_PHASE_FORWARD) {
         const {end, angle} = rotator.getStatus();
         if (end) {
